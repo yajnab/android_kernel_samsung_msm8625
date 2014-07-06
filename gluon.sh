b3e96e106b7c50f4549e5dce85402678d210c47b
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2014-07-06 16:57:28 +0530

    delos: Fixed issues with Packaging and updated buildscript for Alpha-1 release

--- gluon.sh
+++ gluon.sh
@@ -9,11 +9,11 @@ white='tput setaf 7'
 normal='tput sgr0'
 bold='setterm -bold'
 date="date"
-KERNEL_BUILD="Gluon_Kernel_Gingerbread-`date '+%Y-%m-%d---%H-%M'`" 
+KERNEL_BUILD="Gluon_Kernel_delo3geur-`date '+%Y-%m-%d---%H-%M'`" 
 echo $1 > VERSION	
 VERSION='cat VERSION'
 $yellow
-MODULES=./gluon_works/bootimage/boot/lib/modules
+MODULES=./gluon_works/system/lib/modules
 TOOLCHAIN=../../toolchain/linaro/bin/arm-eabi
 $blue
 echo " |========================================================================| "
@@ -71,10 +71,86 @@ clear
 $cyan
 echo "Making the zImage-the real deal"
 $violet
-ARCH=arm CROSS_COMPILE=../../toolchain/linaro/bin/arm-eabi- make -j16 CONFIG_DEBUG_SECTION_MISMATCH=y
-
+ARCH=arm CROSS_COMPILE=../../toolchain/linaro/bin/arm-eabi- make -j64 CONFIG_DEBUG_SECTION_MISMATCH=y
 clear
 $cyan 
+mkdir gluon_works/bootimage/source_img
+echo "Processing the Bootimage"
+cp input_bootimage/boot.img gluon_works/bootimage/source_img/boot.img
+echo "Extraction of the Boot.img"
+$violet
+
+cd gluon_works/bootimage
+rm -rf unpack
+rm -rf output
+rm -rf boot
+clear
+mkdir unpack
+mkdir output
+mkdir boot
+tools/unpackbootimg -i source_img/boot.img -o unpack
+cd boot
+gzip -dc ../unpack/boot.img-ramdisk.gz | cpio -i
+cd ../../../
+$cyan
+echo "Copying output files"
+$violet
+mv arch/arm/boot/zImage boot.img-zImage
+rm gluon_works/bootimage/unpack/boot.img-zImage	
+cp boot.img-zImage gluon_works/bootimage/unpack	
+rm boot.img-zImage
+
+cd gluon_works
+mkdir system
+cd system
+mkdir lib
+cd lib
+mkdir modules
+cd ../../../
+find -name '*.ko' -exec cp -av {} $MODULES/ \;
+
+clear
+
+$cyan
+echo "Stripping Modules"
+$violet
+cd $MODULES
+for m in $(find . | grep .ko | grep './')
+do echo $m
+$TOOLCHAIN-strip --strip-unneeded $m
+done
+clear
+cd ../../../
+
+clear
+$red
+echo " Making boot.img"
+cd bootimage
+$violet
+tools/mkbootfs boot | gzip > unpack/boot.img-ramdisk-new.gz
+rm -rf ../../output/boot.img
+tools/mkbootimg --kernel unpack/boot.img-zImage --ramdisk unpack/boot.img-ramdisk-new.gz -o ../../output/boot.img --base `cat unpack/boot.img-base`	
+rm -rf unpack
+rm -rf output
+rm -rf boot
+cd ../../
+$white
+echo "Making Flashable Zip"
+rm -rf out
+mkdir out
+mkdir out/system
+mkdir out/system/lib
+cp -avr gluon_works/flash/META-INF/ out/
+cp -avr gluon_works/system/lib/modules/ out/system/lib/
+cp output/boot.img out/boot.img
+cd out
+zip -r $KERNEL_BUILD.zip *
+rm -rf META-INF
+rm -rf system
+rm boot.img
+cd ../
+
+
 
 $blue
 echo "Cleaning"
@@ -86,7 +162,6 @@ rm -rf gluon_works/bootimage/boot
 rm -rf gluon_works/bootimage/source_img
 rm VERSION
 clear
-$white
 echo " |============================ F.I.N.I.S.H ! =============================|"
 $red
 echo " |==========================Flash it and Enjoy============================| "
